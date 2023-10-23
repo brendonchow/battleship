76de3b0009bad90bb55d2c6a5ef88921fbba3f25
Brendon Chow <brendonchow123@gmail.com>
2023-10-23 17:41:41 +1100

    Implement player module

---
+++ src/player.js
@@ -0,0 +1,86 @@
+const createPlayer = () => {
+  const attack = (board, pos) => board.receiveAttack(pos);
+  return { attack };
+};
+
+const createPlayerAI = () => {
+  // Store what ships are still alive
+  let shipCoord = null;
+  let shipDirection = null;
+  let direction = 1;
+  let steps = 2;
+  let possible = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  const { attack } = createPlayer();
+  const shipSunk = () => {
+    shipCoord = null;
+    shipDirection = null;
+    direction = 1;
+    steps = 2;
+    possible = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0],
+    ];
+  };
+
+  const aiAttack = (board) => {
+    if (!shipCoord) {
+      // const notAttacked = Array.from(board.notAttacked);
+      // const toHit = JSON.parse(
+      //   notAttacked[Math.floor(Math.random() * notAttacked.length)],
+      // );
+      // if (attack(board, toHit)) shipCoord = toHit;
+      if (attack(board, [1, 0])) {
+        shipCoord = [1, 0];
+      }
+      return;
+    }
+    if (!shipDirection) {
+      let validSquare = false;
+      let toHit;
+      let check;
+      do {
+        check = possible.pop();
+        toHit = [shipCoord[0] + check[0], shipCoord[1] + check[1]];
+        if (toHit[0] <= 9 && toHit[0] >= 0 && toHit[1] <= 9 && toHit[1] >= 0)
+          validSquare = true;
+      } while (!validSquare);
+
+      if (attack(board, toHit)) shipDirection = check;
+      return;
+    }
+    const toHit = [
+      shipCoord[0] + shipDirection[0] * direction * steps,
+      shipCoord[1] + shipDirection[1] * direction * steps,
+    ];
+
+    if (toHit[0] <= 9 && toHit[0] >= 0 && toHit[1] <= 9 && toHit[1] >= 0) {
+      if (attack(board, toHit)) steps += 1;
+      else if (direction === 1) {
+        direction = -1;
+        steps = 1;
+      } else {
+        shipSunk();
+      }
+      return;
+    }
+
+    if (direction === 1) {
+      direction = -1;
+      steps = 1;
+    } else {
+      shipSunk();
+    }
+    aiAttack(board);
+  };
+
+  return { aiAttack };
+};
+
+export { createPlayer, createPlayerAI };
---
+++ src/player.test.js
@@ -0,0 +1,58 @@
+import createGameBoard from "./gameboard";
+import { createPlayer, createPlayerAI } from "./player";
+
+const player = createPlayer();
+const gameBoard = createGameBoard();
+
+test("Test player sends receiveAttack to gameBoard", () => {
+  player.attack(gameBoard, [0, 0]);
+
+  expect(gameBoard.notAttacked.has(`[${0},${0}]`)).toBe(false);
+  expect(gameBoard.checkAllShipsSunk()).toBe(true);
+});
+
+const player2 = createPlayerAI();
+const gameBoard2 = createGameBoard();
+gameBoard2.placeShip(5, [1, 0], [2, 0]);
+
+test("aiAttack", () => {
+  expect(gameBoard2.checkAllShipsSunk()).toBe(false);
+
+  player2.aiAttack(gameBoard2); // [1, 0]
+  player2.aiAttack(gameBoard2); // [0, 0]
+  player2.aiAttack(gameBoard2); // [2, 0]
+  player2.aiAttack(gameBoard2); // [3, 0]
+  player2.aiAttack(gameBoard2); // [4, 0]
+  player2.aiAttack(gameBoard2); // [5, 0]
+  expect(gameBoard2.checkAllShipsSunk()).toBe(true);
+  player2.aiAttack(gameBoard2);
+  expect(gameBoard2.notAttacked.size).toBe(93);
+});
+
+const player3 = createPlayerAI();
+const gameBoard3 = createGameBoard();
+gameBoard3.placeShip(4, [0, 0], [1, 0]);
+
+test("aiAttack both directions", () => {
+  expect(gameBoard3.checkAllShipsSunk()).toBe(false);
+
+  player3.aiAttack(gameBoard3);
+  player3.aiAttack(gameBoard3);
+  player3.aiAttack(gameBoard3);
+  player3.aiAttack(gameBoard3);
+  expect(gameBoard3.checkAllShipsSunk()).toBe(true);
+});
+
+const player4 = createPlayerAI();
+const gameBoard4 = createGameBoard();
+gameBoard4.placeShip(2, [1, 0], [1, 1]);
+
+test("aiAttack checks all directions", () => {
+  expect(gameBoard4.checkAllShipsSunk()).toBe(false);
+
+  player4.aiAttack(gameBoard4);
+  player4.aiAttack(gameBoard4);
+  player4.aiAttack(gameBoard4);
+  player4.aiAttack(gameBoard4);
+  expect(gameBoard4.checkAllShipsSunk()).toBe(true);
+});
